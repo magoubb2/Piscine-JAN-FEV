68b17df7cb674280ff3da617229a36f5bc27b7c4
magoubb2 <margueritebb@hotmail.ca>
2023-12-01 17:53:05 -0500

    added comment ex05

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -1,16 +1,37 @@
 #include <unistd.h>
 
-// 
+// 1. First we initialise 3 int (a, b and c) because the bigest 
+// number is composed is a 3 digits number. 
+// Then we initialise an array of char (comb) of size 3. This array
+// holds the character representation of the 3 digits:
+// comb[3] -> [0][1][2]
+//		   -> [a][b][c]
+// The "while (a <= 7)" continue as long as a is equal to 7.
+// same for all of the others loops.
+// 'b' is set to the next value that a is set to so there is
+// never duplicate of numbers.
+// Things to look out for:
+// 1. No comma and space after last num.
+// 2. Check if your output is right.
+
+void print_comb(int a, int b, int c)
+{
+	char comb[3];
+
+	comb[0] = a + 48;
+	comb[1] = b + 48;
+	comb[2] = c + 48;
+
+	write(1, comb, 3);
+}
 
 void ft_print_comb(void)
 {
     int a;
     int b;
     int c;
-    char comb[3];
 
     a = 0;
-
     while (a <= 7)
     {
         b = a + 1;
@@ -19,12 +40,9 @@ void ft_print_comb(void)
             c = b + 1;
             while (c <= 9)
             {
-                comb[0] = a + 48;
-                comb[1] = b + 48;
-                comb[2] = c + 48;
-                write(1, comb, 3);
-                if (a != 7 || b != 8 || c != 9)
-                    write(1, ", ", 2);
+				print_comb(a, b, c);
+            	if (a != 7 || b != 8 || c != 9)
+                	write(1, ", ", 2);
                 c++;
             }
             b++;
