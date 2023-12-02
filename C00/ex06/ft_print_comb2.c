b22b159277c0a4e92b722b6b8c686bffbdcf5257
margo <margueritebaronbeliveau@marguerites-MacBook-Air.local>
2023-12-01 22:01:10 -0500

    fixed ft_print_comb2 + check if rly understood

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -1,54 +1,44 @@
 #include <unistd.h>
 
-void comb(int a, int b, int c, int d)
+void coma_space(int a, int b)
+{
+    if (a != 98 || b != 99)
+        write(1, ", ", 2);
+}
+
+void comb(int a, int b)
 {
     char comb[5];
 
-    comb[0] = a + 48;
-    comb[1] = b + 48;
+    comb[0] = (a / 10) + 48;
+    comb[1] = (a % 10) + 48;
     comb[2] = ' ';
-    comb[3] = c + 48;
-    comb[4] = d + 48;
+    comb[3] = (b / 10) + 48;
+    comb[4] = (b % 10) + 48;
 
     write(1, comb, 5);
+    coma_space(a, b);
 }
 
-void coma_space(int a, int b, int c, int d)
-{
-    if (a != 9 || b != 8 || c != 9 || d != 9)
-        write(1, ", ", 2);
-}
 
 void ft_print_comb2(void)
 {
     int a;
     int b;
-    int c;
-    int d;
 
     a = 0;
-    while (a <= 9)
+    while (a <= 98)
     {
-        b = 0;
-        while (b <= 8)
+        b = a + 1;
+        while (b <= 99)
         {
-            c = a;
-            while (c <= 9)
-            {
-                d = b + 1;
-                while (d <= 9)
-                {
-                    comb(a, b, c, d);
-                    coma_space(a, b, c, d);
-                    d++;
-                }
-                c++;
-            }
+            comb(a, b);
             b++;
         }
         a++;
     }
 }
+
 
 int main(void)
 {
