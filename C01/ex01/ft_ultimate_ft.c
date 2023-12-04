dbb9efa3b606610b10a3ff52736f1bf868bdd29c
margo <margueritebaronbeliveau@marguerites-MacBook-Air.local>
2023-12-04 04:38:19 -0500

    ft_ultiimate_ft(done)

--- C01/ex01/ft_ultimate_ft.c
+++ C01/ex01/ft_ultimate_ft.c
@@ -1,19 +1,34 @@
 
 #include <stdio.h>
 
-// void ft_ultimate_ft(int *********nbr)
-// {
-// 	int i;
-
-// 	i = 0;
-// 	********nbr = nbr;
-// }
+void ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
 
 int main(void)
 {
-	int i;
-	int *p;
- 
-	p = &i;
-	printf("%p\n", p);
+	int value;
+	int *ptr1;
+	int **ptr2;
+	int ***ptr3;
+	int ****ptr4;
+	int *****ptr5;
+	int ******ptr6;
+	int *******ptr7;
+	int ********ptr8;
+	int *********nbr;
+
+	ptr1 = &value;
+	ptr2 = &ptr1;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	nbr = &ptr8;
+
+	ft_ultimate_ft(nbr);
+	printf("%i\n", *********nbr);
 }
