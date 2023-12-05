077bb193215e81694422f7ee96feb15a7e6691fb
magoubb2 <margueritebb@hotmail.ca>
2023-12-04 19:17:17 -0500

    ft_swap(comment)

--- C01/ex02/ft_swap.c
+++ C01/ex02/ft_swap.c
@@ -1,13 +1,35 @@
 
 #include <stdio.h>
 
-void ft_swap(int *a, int *b)
+void ft_swap(int *v_a, int *v_b)
 {
-    int tmp;
+    int tmp = 0;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+    printf("tmp avant = %i\n", tmp);
+	printf("\n");
+    printf("v_a avant = %d\n", *v_a);
+	printf("\n");
+    printf("v_b avant = %d\n", *v_b);
+	printf("\n");
+	printf("adress v_b: %p\n", &v_b);
+	printf("\n");
+	printf("adress v_a: %p\n", &v_a);
+	printf("\n");
+
+    tmp = *v_a;
+    printf("tmp après à l'adresse : [%p] = <tmp = *v_a> = %i\n", &tmp, tmp);
+	printf("\n");
+    printf("v_a après à l'adresse : [%p] = <tmp = *v_a> = %d\n", &v_a, *v_a);
+    *v_a = *v_b;
+	printf("\n");
+    printf("v_a après à l'adresse : [%p] = <*v_a = *v_b> = %d\n", &v_a, *v_a);
+	printf("\n");
+    printf("v_b après à l'adresse : [%p] = <*v_a = *v_b> = %d\n", &v_b, *v_b);
+	printf("\n");
+    *v_b = tmp;
+    printf("v_b après à l'adresse : [%p] = <*v_b = tmp> = %d\n", &v_b, *v_b);
+	printf("\n");
+    printf("v_a après à l'adresse : [%p] = <*v_b = tmp> = %d\n", &v_a, *v_a);
 }
 
 int main(void)
@@ -16,8 +38,20 @@ int main(void)
     int b = 60;
 
     printf("avant %i\n", a);
-    printf("apres %i\n", b);
+	printf("\n");
+    printf("adresse %p\n", &a);
+	printf("\n");
+    printf("avant %i\n", b);
+	printf("\n");
+    printf("avant adresse %p\n", &b);
+	printf("\n");
     ft_swap(&a, &b);
-    printf("avant %i\n", a);
-    printf("apres %i\n", b);
+	printf("\n");
+    printf("après %i\n", a);
+	printf("\n");
+    printf("après adresse %p\n", &a);
+	printf("\n");
+    printf("après %i\n", b);
+	printf("\n");
+    printf("après adresse %p\n", &b);
 }
