d56fc67b93710c8945a34d9ba82bec0973a89ad3
magoubb2 <margueritebb@hotmail.ca>
2023-12-05 13:25:58 -0500

    ft_putstr(done)

---
+++ C01/ex05/ft_putstr.c
@@ -0,0 +1,21 @@
+
+#include <unistd.h>
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int main(void)
+{
+	char *str = "hello world 12345678";
+
+	ft_putstr(str);
+}
