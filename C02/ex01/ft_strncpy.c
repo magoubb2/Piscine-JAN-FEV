59ce9016ccf4ae6dd683f594277f9ef31c7ba271
magoubb2 <margueritebb@hotmail.ca>
2023-12-06 17:07:55 -0500

    ft_strncpy(re-done)

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -2,25 +2,34 @@
 #include <stdio.h>
 #include <string.h>
 
+// copy from src to dest up until n.
+#define SIZE 15
+
 char *ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	i = 0;
-	while (i != n)
+	while (src[i] && i < n)
 	{
+		printf("[%i] = %s\n", i, dest);
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
 
 int main(void)
 {
-	char dest[] = "hello";
-	char src[] = "world";
-	unsigned int n = 5;
+	const unsigned int n = 15;
+	char dest[n];
+	bzero(dest, n);
+	char src[] = "world yoyololo";
 
 	printf("%s\n", ft_strncpy(dest, src, n));
 	printf("%s\n", strncpy(dest, src, n));
