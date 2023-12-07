3bbd8f044c03b67afaa6d609d8f314975c37a890
magoubb2 <margueritebb@hotmail.ca>
2023-12-07 15:29:40 -0500

    (need)finish ex09

---
+++ C02/ex09/ft_strcapitalize.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+
+char *ft_strcapitalize(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] == ' ')
+		i++;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32;
+		i++;
+	}
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] = str[0] - 32;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == ' ')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] = str[i + 1] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+int main(void)
+{
+	char str[] = "         BLABLA+BLABLA.";
+
+	printf("%s\n", ft_strcapitalize(str));
+}
