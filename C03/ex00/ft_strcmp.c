e7ed465e6645a6563c4b9236a3b39435812a7836
magoubb2 <margueritebb@hotmail.ca>
2023-12-11 12:33:48 -0500

    ft_strcmp(done+comments)

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
+// Loop by checking char by char until the char are not equal.
+// If the char are not equal the difference between the two char
+// is returned(in ascii).
+// Exemple if the char in s1 is 'l' in ascii->(108) and s2 is 7
+// in ascii->(55) then the difference is '53'.
+// If s1 and s2 are identicall until the '\0' is reached,
+// we return '0'.
 int ft_strcmp(char *s1, char *s2)
 {
 	int i;
@@ -19,7 +26,7 @@ int ft_strcmp(char *s1, char *s2)
 int main(void)
 {
 	char str1[] = "hello";
-	char str2[] = "ell7";
+	char str2[] = "hel7l7";
 
 	printf("%i\n", ft_strcmp(str1, str2));
 	printf("%i\n", strcmp(str1, str2));
