6f59028c86331669df195c61ec26c0ed279e5580
margo <margueritebaronbeliveau@marguerites-MacBook-Air.local>
2023-12-12 12:04:59 -0500

    ft_strcat(done)

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -2,6 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 
+// We concatenate two string, "src" and "dest". It appends the character of the src 
+// to the end of the dest string. We calculate the lenght of dest and src. We then use
+// ft_memcpy to copy the character from the src to the end of the dest string.
+// So for exemple, i have : dest[] = "hello" 
+//							dest_len = 5                                      
+//							dest + dest_len = [0]'h'[1]'e'[2]'l'[3]'l'[4]'o'[5]'\0'
+// So this this is where we start to concatenate src to dest				 ^
+// This condition-> (dest + dest_len) bring us to the null terminated char of dest.
+// So we can start to concatenate at the end of the dest string. And this condition,
+// -> (src_len + 1) is so we include the null terminated char of src.
+char *ft_strcat(char *dest, char *src);
+
 int ft_strlen(char *str)
 {
 	int i;
