675befcd03a6a4755d3b74f487c409f4e7fa28d5
mabaron- <margueritebaronbeliveau@marguerites-Laptop.local>
2023-12-16 11:31:04 -0500

    ft_recursive_power(done)

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int ft_recursive_power(int nb, int power)
+{
+    if (power == 0)
+        return (1);
+    else
+        return (nb * ft_recursive_power(nb, power - 1));
+}
+
+int main(void)
+{
+    printf("%i\n", ft_recursive_power(8, 3));
+}
