a85ea7ec6a50c36bc469a4de4b4e7f325023a93c
mabaron- <margueritebaronbeliveau@marguerites-Laptop.local>
2023-12-16 12:58:52 -0500

    ft_fibonacci(recursive)

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -1,26 +1,36 @@
 
 #include <stdio.h>
 
+// int ft_fibonacci(int index)
+// {
+//     int i;
+//     int a;
+//     int b;
+//     int c;
+
+//     a = 0;
+//     b = 1;
+//     i = 2;
+//     if (index == 0)
+//         return (a);
+//     while (i <= index)
+//     {
+//         c = a + b;
+//         a = b;
+//         b = c;
+//         i++;
+//     }
+//     return (b);
+// }
+
 int ft_fibonacci(int index)
 {
-    int i;
-    int a;
-    int b;
-    int c;
-
-    a = 0;
-    b = 1;
-    i = 2;
     if (index == 0)
-        return (a);
-    while (i <= index)
-    {
-        c = a + b;
-        a = b;
-        b = c;
-        i++;
-    }
-    return (b);
+        return (0);
+    if (index == 1)
+        return (1);
+    else
+        return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
 
 int main(void)
