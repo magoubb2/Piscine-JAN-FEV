d42a33a7ba9cfe6df247e3a22e84e89be6a341f9
mabaron- <margueritebaronbeliveau@marguerites-Laptop.local>
2023-12-16 16:02:43 -0500

    ft_fibonacci(need comment + check more about recursive)

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -25,15 +25,16 @@
 
 int ft_fibonacci(int index)
 {
+    if (index < 0)
+        return (-1);
     if (index == 0)
         return (0);
     if (index == 1)
         return (1);
-    else
-        return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+    return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
 
 int main(void)
 {
-    printf("%i\n", ft_fibonacci(0));
+    printf("%i\n", ft_fibonacci(8));
 }
