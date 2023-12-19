a3f44ade2549b14d73cc87bb35a2a209fdba1e32
mabaron- <margueritebaronbeliveau@marguerites-Laptop.local>
2023-12-19 15:07:45 -0500

    ft_fibonacci(added comment)

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -1,40 +1,26 @@
 
 #include <stdio.h>
 
-// int ft_fibonacci(int index)
-// {
-//     int i;
-//     int a;
-//     int b;
-//     int c;
-
-//     a = 0;
-//     b = 1;
-//     i = 2;
-//     if (index == 0)
-//         return (a);
-//     while (i <= index)
-//     {
-//         c = a + b;
-//         a = b;
-//         b = c;
-//         i++;
-//     }
-//     return (b);
-// }
-
+// Calculates the Fibonacci number at a given index using recursion.
+// Recursive exemple :
+// f6
+// f5 + f4
+// f4 + f3 f3 + f2
+// f3 + f2 f2 + f(1) f2 + (1) (1) + 0
+// f2 + (1) (1) + 0 (1) + 0 (1) + 0 
+// (1) + 0
+// result: 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 = 8.
+// If index is less than 2 we return index. Else we continue the recursive.
 int ft_fibonacci(int index)
 {
     if (index < 0)
         return (-1);
-    if (index == 0)
-        return (0);
-    if (index == 1)
-        return (1);
+    if (index < 2)
+        return (index);
     return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
 
 int main(void)
 {
-    printf("%i\n", ft_fibonacci(8));
+    printf("%i\n", ft_fibonacci(6));
 }
