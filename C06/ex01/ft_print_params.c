c88e428aa5e0d6046979685c0151a2ee04dadfee
mabaron- <margueritebaronbeliveau@marguerites-Laptop.local>
2023-12-19 11:03:06 -0500

    ft_print_params(comment added)

--- C06/ex01/ft_print_params.c
+++ C06/ex01/ft_print_params.c
@@ -1,6 +1,18 @@
 
 #include <unistd.h>
 
+// This function prints each command-line arguments excluding the programm
+// itself. 
+// (i) is for iterating trougth each command-line arguments.
+//          -> so (i) is given the value of '1' because we want to exclude 
+//             the programm itself.
+// (j) is for iterating trougth character in each arguments.
+//          -> (j) is given the value of '0' inside of the first while loop
+//             to initialise the counter for the current argument.
+// The first while loop continue until (i) is no longer less than (argc) so
+// while (i) is less than (argc) we increment (i),
+// The second loop write each character of the current argument  until  the 
+// NULL character is reach.
 int main(int argc, char **argv)
 {
     int i;
