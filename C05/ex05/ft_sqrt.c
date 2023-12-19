4a23e14dead46168ab9e5bcce6cc8d7a02030b84
mabaron- <margueritebaronbeliveau@marguerites-Laptop.local>
2023-12-19 15:29:42 -0500

    ft_sqrt(added comment

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -1,6 +1,21 @@
 
 #include <stdio.h>
 
+// Calculate the square root of a given non-negative integer.
+// The while loop continue until (x) is no longer more than y.
+// Exemple : 64
+// 64 = (64 + 1) / 2 -> 32
+// 1 = 64 / 32 -> 2
+//
+// 32 = (32 + 1) / 2 -> 16
+// 2 = 64 / 16 -> 4
+//
+// 16 = (16 + 1) / 2 -> 8
+// 4 = 64 / 8 -> 8
+//
+// (x) is no longer more than y. now we need to check if 8 is really
+// the square root of 64 by doing (x) * (x),  which means -> (x^2).
+// and inded 8 * 8 = 64 so we return (x) which is 8 in this case.
 int ft_sqrt(int nb)
 {
     int x;
