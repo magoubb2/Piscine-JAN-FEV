eb18a416872d431fa7260c6f0d0c9cf1f977b184
mabaron- <margueritebaronbeliveau@marguerites-Laptop.local>
2023-12-23 11:47:39 -0500

    ft_strjoin(done + added comments)

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Step 1). We use ft_strlen to find the length of the (strs), so we calculate the
+// length of each string in (**strs).
+// Step 2). Then we had to the length, the length of the (sep) times the number of
+// string in the (**strs) array of string.
+// Step 3). We then initialise a new variable  called  result to which we allocate
+// the length of each string in (strs) + the (sep) times the number of string  + 1
+// for the ('\0') the NULL terminating character.
+// Step 4). We do the malloc check + we insure that result is empty and full  of 0
+// so we can avoid garbages value...
+// Step 5). We finially concatenate (strs) to (result) + add the (sep) in  between
+// each words by concatening (sep) to (result).
+// Step 6). And we return (result).
+char *ft_strjoin(int size, char **strs, char *sep);
+
 int ft_strlen(char *str)
 {
     int i;
@@ -58,7 +72,7 @@ char *ft_strjoin(int size, char **strs, char *sep)
     while (i < size)
     {
         ft_strcat(result, strs[i]);
-        if (i < size - 1)
+        if (i < (size - 1))
             ft_strcat(result, sep);
         i++;
     }
